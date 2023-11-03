1131e79bdd1e73f2da65f03e83180f5e4215407b
Jason Bobo Kyaw <jasonbobokyaw@gmail.com>
2023-11-03 23:21:42 +0800

    deployment testing

--- seller/seller_recommender.py
+++ seller/seller_recommender.py
@@ -1,5 +1,6 @@
-import sys
 import os
+os.environ['SURPRISE_DATA_FOLDER'] = '/home/site/wwwroot/surprise'
+import sys
 from surprise import Dataset, Reader
 from surprise.model_selection import train_test_split
 from surprise import SVD
