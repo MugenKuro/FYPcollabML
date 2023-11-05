c65a2af032629828218fa86425187bbd307c8b7a
MugenKuro <hoonzhenyi@gmail.com>
2023-11-05 10:35:54 +0800

    Add files via upload

--- registerCustomer.php
+++ registerCustomer.php
@@ -1,6 +1,7 @@
 <?php
 // Include file
 require_once dirname(__FILE__) . '/entity/users.php';
+require_once dirname(__FILE__) . '/controller/userController.php';
 require_once('auth.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
