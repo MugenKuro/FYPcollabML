6734fa28b6aee3713ca24f6c6df5bb614950db90
FaustineL <115613284+FaustineL@users.noreply.github.com>
2023-11-07 16:04:19 +0800

    Add files via upload

--- seller/sellerEditSettings.php
+++ seller/sellerEditSettings.php
@@ -1,90 +1,13 @@
-<!doctype html>
-<html lang="en">
 <?php
 require_once __DIR__ . '/../entity/users.php';
 require_once __DIR__ . '/../controller/sellerController.php';
-//require_once __DIR__ . '/../auth.php';
+require_once __DIR__ . '/../auth.php';
 require_once __DIR__ . '/../sellerAuth.php';
-
-
-if (isset($_POST['requestDeactivation'])){
-    $sellerController = new sellerController;
-    $deactivate = $sellerController->deactivateUser();
-}
-
-if (isset($_POST['updateLogin'])){
-    $is_image_uploaded = !empty($_FILES["profile_image"]["name"]);
-			
-    if ($is_image_uploaded) {
-        $target_dir = "../images/SellerLogo/";		
-        $target_file = $target_dir . basename($_FILES["profile_image"]["name"]);
-        $uploadsuccess = 1;
-        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-        
-        $check = getimagesize($_FILES["profile-image"]["tmp_name"]);
-        if ($check === false) {
-            echo "File is not an image.";
-            $uploadsuccess = 0;
-        }
-        
-        if ($_FILES["profile_image"]["size"] > 500000) {
-            echo "Sorry, your file is too large.";
-            $uploadsuccess = 0;
-        }
-        
-        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-            echo "Sorry, only JPG,, JPEG & PNG files are allowed.";
-            $uploadsuccess = 0;
-        }
-        
-        if ($uploadsuccess == 0) {
-            echo "Sorry, your file was not uploaded.";
-        } else {
-            if (move_uploaded_file($_FILES["profile_image"]["tmp_name"], $target_file)) {
-                echo "The file " . htmlspecialchars(basename($_FILES["profile_image"]["name"])) . " has been uploaded..";
-                $profile_image = $target_file;
-            } else {
-                echo "Sorry, there was an error uploading your file.";
-                
-            }
-        }
-    } else {
-         $profile_image = isset($_POST['profile_image']) ? $_POST['profile_image'] : $profile_image;
-    }
-    $username= $_POST['username'];
-    $password1 = $_POST['password1'];
-    $password2 = $_POST['password2']; 
-    $sellerName = $_POST['seller_name']; //seller
-    $description = $_POST['description']; //seller
-    $preferred_category = $_POST['preferred_category'];
-    $address1 = $_POST['address1']; //seller
-    $address2 = $_POST['address2']; //seller
-    $address3 = $_POST['address3']; //seller
-    $address = $address1 . ',' . $address2 . ',' . $address3;
-    $email = $_POST['email'];
-    $bank_name = $_POST['bank_name'];
-    $bank_account_no = $_POST['bank_account_no'];
-    
-    
-    $sellerController = new sellerController;
-        $result = $sellerController ->editSettings([
-            'profile_image' => $profile_image,
-            'username' => $username,
-            'password1' => $password1,
-            'password2'=> $password2,
-            'seller_name'=> $sellerName,
-            'description'=> $description,
-            'pick_up_address'=> $address,
-            'email' => $email,
-            'preferred_category' => $preferred_category,
-            'bank_name'=> $bank_name,
-            'bank_account_no'=> $bank_account_no
-
-        ]);
-            header('Location: sellerAccountSetting.php');
-}
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -106,12 +29,90 @@ if (isset($_POST['updateLogin'])){
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-
     <title>iCloth</title>
 </head>
 
 <body>
+    <?php
+    include dirname(__FILE__) . ('/sellerNavBar.php');
 
+    if (isset($_POST['requestDeactivation'])){
+        $sellerController = new sellerController;
+        $deactivate = $sellerController->deactivateUser();
+    }
+    
+    if (isset($_POST['updateLogin'])){
+        $is_image_uploaded = !empty($_FILES["profile_image"]["name"]);
+                
+        if ($is_image_uploaded) {
+            $target_dir = "../images/SellerLogo/";		
+            $target_file = $target_dir . basename($_FILES["profile_image"]["name"]);
+            $uploadsuccess = 1;
+            $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+            
+            $check = getimagesize($_FILES["profile-image"]["tmp_name"]);
+            if ($check === false) {
+                echo "File is not an image.";
+                $uploadsuccess = 0;
+            }
+            
+            if ($_FILES["profile_image"]["size"] > 500000) {
+                echo "Sorry, your file is too large.";
+                $uploadsuccess = 0;
+            }
+            
+            if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+                echo "Sorry, only JPG,, JPEG & PNG files are allowed.";
+                $uploadsuccess = 0;
+            }
+            
+            if ($uploadsuccess == 0) {
+                echo "Sorry, your file was not uploaded.";
+            } else {
+                if (move_uploaded_file($_FILES["profile_image"]["tmp_name"], $target_file)) {
+                    echo "The file " . htmlspecialchars(basename($_FILES["profile_image"]["name"])) . " has been uploaded..";
+                    $profile_image = $target_file;
+                } else {
+                    echo "Sorry, there was an error uploading your file.";
+                    
+                }
+            }
+        } else {
+             $profile_image = isset($_POST['profile_image']) ? $_POST['profile_image'] : $profile_image;
+        }
+        $username= $_POST['username'];
+        $password1 = $_POST['password1'];
+        $password2 = $_POST['password2']; 
+        $sellerName = $_POST['seller_name']; //seller
+        $description = $_POST['description']; //seller
+        $preferred_category = $_POST['preferred_category'];
+        $address1 = $_POST['address1']; //seller
+        $address2 = $_POST['address2']; //seller
+        $address3 = $_POST['address3']; //seller
+        $address = $address1 . ',' . $address2 . ',' . $address3;
+        $email = $_POST['email'];
+        $bank_name = $_POST['bank_name'];
+        $bank_account_no = $_POST['bank_account_no'];
+        
+        
+        $sellerController = new sellerController;
+            $result = $sellerController ->editSettings([
+                'profile_image' => $profile_image,
+                'username' => $username,
+                'password1' => $password1,
+                'password2'=> $password2,
+                'seller_name'=> $sellerName,
+                'description'=> $description,
+                'pick_up_address'=> $address,
+                'email' => $email,
+                'preferred_category' => $preferred_category,
+                'bank_name'=> $bank_name,
+                'bank_account_no'=> $bank_account_no
+    
+            ]);
+                header('Location: sellerAccountSetting.php');
+    }
+    ?>
     <div>
         <div class="seller-container">
             <div class="seller-container01">
--- seller/sellerRequestCategory.php
+++ seller/sellerRequestCategory.php
@@ -1,13 +1,12 @@
-<!doctype html>
-<html lang="en">
 <?php
 require_once __DIR__ . '/../entity/users.php';
 require_once __DIR__ . '/../controller/sellerController.php';
-//require_once __DIR__ . '/../auth.php';
+require_once __DIR__ . '/../auth.php';
 require_once __DIR__ . '/../sellerAuth.php';
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
@@ -17,7 +16,7 @@ require_once __DIR__ . '/../sellerAuth.php';
 
     <meta name="description" content="" />
     <meta name="keywords" content="bootstrap, bootstrap4" />
-    
+
     <!-- Bootstrap CSS -->
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
@@ -30,16 +29,18 @@ require_once __DIR__ . '/../sellerAuth.php';
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-
     <title>iCloth</title>
 </head>
 
-<body>    
+<body>
+    <?php
+    include dirname(__FILE__) . ('/sellerNavBar.php');
+    ?>
     <div class="seller-container">
         <div class="seller-container01">
             <div class= "centering-div">
             <?php
-    include dirname(__FILE__) . ('/sellerNavBar.php');
+
     if(isset($_POST["requestCategory"])){
         $category = $_POST['category'];
         $sellerController = new sellerController();
