3fbd733bea535e234680404ba1c35faabc31bc2d
darinatic <jasonbobokyaw@gmail.com>
2023-11-10 13:52:14 +0800

    seller_inventory

--- seller/view_inventory.php
+++ seller/view_inventory.php
@@ -75,6 +75,7 @@ try {
                     <link rel="stylesheet" href="../css/bootstrap.min.css">
                     <link rel="stylesheet" href="../css/style.css">
                     <script src="../js/jquery-3.7.1.min.js"></script>
+                    <script src="../js/popper.min.js"></script>
                 </head>
                 <body>               
                     <div class="container mt-4">
