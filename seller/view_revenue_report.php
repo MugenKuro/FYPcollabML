7f1098768f074480254e5f8d140c43af8de8628b
darinatic <jasonbobokyaw@gmail.com>
2023-11-14 18:04:14 +0800

    fix

--- seller/view_revenue_report.php
+++ seller/view_revenue_report.php
@@ -90,9 +90,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['generate'])) {
 <!DOCTYPE html>
 <html>
 <head>
-    <!-- <title>Seller Orders Report</title> -->
-    <!-- <link rel="stylesheet" href="../css/bootstrap.min.css"> -->
-    <!-- <link rel="stylesheet" href="../css/style.css"> -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Untree.co">
+    <link rel="shortcut icon" href="favicon.png">
+
+    <meta name="description" content="" />
+    <meta name="keywords" content="bootstrap, bootstrap4" />
+
+    <!-- Bootstrap CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link href="../css/tiny-slider.css" rel="stylesheet">
+    <link href="../css/sellerStyle.css" rel="stylesheet">
+
+
+    <!-- Include Bootstrap JavaScript and jQuery (required for dropdown functionality) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <title>iCloth</title>
+
     <style>
         .a {
             padding: 20px;
@@ -128,7 +147,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['generate'])) {
             display: flex;
             justify-content: flex-start;
             align-items: flex-end;
-            height: 100%;
         }
         .btn-generate {
             position: relative; 
