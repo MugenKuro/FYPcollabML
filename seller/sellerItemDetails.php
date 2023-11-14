52df60f05e6d7de681ec05dd2d5f8aec42464e15
FaustineL <115613284+FaustineL@users.noreply.github.com>
2023-11-14 20:55:39 +0800

    Add files via upload

--- seller/sellerItemDetails.php
+++ seller/sellerItemDetails.php
@@ -13,37 +13,6 @@ require_once __DIR__ . '/../sellerAuth.php';
     <meta name="author" content="Untree.co">
     <link rel="shortcut icon" href="favicon.png">
 
-    <style>
-        .button-column{
-            float: left;
-            width: 33.33%;
-            padding: 10px;
-        }
-        .button-separator:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-.Dropdown-Inventory {
-  display: block;
-  width: auto;
-  padding: 0.375rem 0.75rem;
-  font-size: 1rem;
-  font-weight: 400;
-  line-height: 1.5;
-  color: #212529;
-  background-color: #fff;
-  background-clip: padding-box;
-  border: 1px solid #ced4da;
-  -webkit-appearance: none;
-     -moz-appearance: none;
-          appearance: none;
-  border-radius: 0.375rem;
-  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-}
-    </style>
-
     <meta name="description" content="" />
     <meta name="keywords" content="bootstrap, bootstrap4" />
 
@@ -51,7 +20,7 @@ require_once __DIR__ . '/../sellerAuth.php';
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link href="../css/tiny-slider.css" rel="stylesheet">
-    <link href="../css/style.css" rel="stylesheet">
+    <link href="../css/sellerStyle.css" rel="stylesheet">
 
 
     <!-- Include Bootstrap JavaScript and jQuery (required for dropdown functionality) -->
@@ -63,26 +32,25 @@ require_once __DIR__ . '/../sellerAuth.php';
 </head>
 
 <body>
-
-    <?php
-    include dirname(__FILE__) . ('/sellerNavBar.php');
-
-    if(isset($_GET['item_id'])) {
+	
+  <?php
+  
+	if(isset($_GET['item_id'])) {
 		
         $item_id = $_GET['item_id'];
 		$sellerController = new sellerController;
-		$itemData = $sellerController -> getItemData($item_id);
-			$row = mysqli_fetch_assoc($itemData);
-        $sellerData = $sellerController -> getSellerData($item_id);
-            $row2 = mysqli_fetch_assoc($sellerData);
-        $itemAverageRating = $sellerController->getItemAverage($item_id);
-            $row3= mysqli_fetch_assoc($itemAverageRating);
-        $itemRating = $sellerController ->getItemReviews($item_id);
-        if($itemRating) {
-            $row4= mysqli_fetch_assoc($itemRating);
-        }
-        $itemInventory= $sellerController->getInventory($item_id);
-	}
+        $itemData = $sellerController -> getItemData($item_id);
+        $row = mysqli_fetch_assoc($itemData);
+    $sellerData = $sellerController -> getSellerData($item_id);
+        $row2 = mysqli_fetch_assoc($sellerData);
+    $itemAverageRating = $sellerController->getItemAverage($item_id);
+        $row3= mysqli_fetch_assoc($itemAverageRating);
+    $itemRating = $sellerController ->getItemReviews($item_id);
+    if($itemRating) {
+        $row4= mysqli_fetch_assoc($itemRating);
+    }
+    $itemInventory= $sellerController->getInventory($item_id);
+}
 
     if(isset($_POST["deleteItem"]))
     {
@@ -104,33 +72,35 @@ require_once __DIR__ . '/../sellerAuth.php';
 			$result = $search;
 		}
 	}
+	?>
+	
+    <?php
+    include dirname(__FILE__) . ('/sellerNavBar.php');
     ?>
 	
-    
     <div>
-        <link href="./view-item.css" rel="stylesheet" />
-        <div class="view-item-container">
+    <div class="view-item-container">
             <div class="view-item-container01">
                 <div id="message"></div>
                 <div class="view-item-container02">
-                    <img alt="image" src="..<?php echo $row['item_image_path']?>" class="view-item-image" />
-                    <div class="view-item-container03">
-                        <div class="view-item-container04">
-                            <span class="view-item-text">
-                                <span class="view-item-text01">
-                                <?=$row['item_name'] ?>
-                                </span>
-                                <br />
-                            </span>
-                            <span class="view-item-text03">
-                                <span>$</span>
-                                <span>
-                                <?=$row['price'] ?>
-                                </span>
-                                <br />
-                            </span>
-                        </div>
-                        <div class="view-item-container05">
+            <?php 
+            if($itemData)
+            {    
+            ?>
+            <img alt="image" class ="view-item-image" src="..<?php echo $row['item_image_path']?>"></img>
+            <div class="view-item-container03">
+            <div class="view-item-container04">
+            <span class="view-item-text">
+            <span class = "item-details-text-underline">Item Name :</span>
+            <br>    
+            </span>
+            <span class = "item-details-text"><?=$row['item_name'] ?></span>
+            <span class="view-item-text03">
+                <span>$</span>
+                    <span><?=$row['price'] ?></span><br />
+                </span>
+            </div>
+            <div class="view-item-container05">
                             <div class="view-item-container06">
                                 <img alt="image" src="..<?php echo $row2["profile_image"] ?>" class="view-item-image1" />
                                 <div class="view-item-container07">
@@ -141,9 +111,7 @@ require_once __DIR__ . '/../sellerAuth.php';
                                     </span>
                                 </div>
                             </div>
-                            <div class="view-item-container08">
-                                <form action="" class="add-form-submit">
-                                    <div class="col-sm-6">
+                            <div class="col-sm-6">
                                         <label for="size">Size</label>
                                         <select id="size" name="size" class=" Dropdown-Inventory"
                                             style="margin-bottom: 5px;">
@@ -157,66 +125,51 @@ require_once __DIR__ . '/../sellerAuth.php';
                                                 }
                                             ?>
                                         </select>
-                                    </div>
-                                    <input type="hidden" class="added_id" value="<?= $item_id ?>">
-                                    <input type="hidden" class="added_price" value="<?= $itemPrice ?>">
-                                    <input type="hidden" class="added_qty" value="1">
-                                </form>
                             </div>
                         </div>
                     </div>
+
                     <div class="view-item-container09"></div>
                     <div class="view-item-container10">
                         <span class="view-item-text16">
-                            <span class="view-item-text17">Category</span>
-                            <br />
+                            <span class="view-item-text17">Category</span><br />
                         </span>
                         <span class="view-item-text19">
-                            <span>
-                            <?php echo $row['category_name']; ?>
-                            </span>
+                            <span><?php echo $row['category_name']; ?></span>
                             <br />
                         </span>
                     </div>
                     <div class="view-item-container11">
                         <span class="view-item-text22">
-                            <span class="view-item-text23">Description</span>
-                            <br />
+                            <span class="view-item-text23">Description</span><br /> 
                         </span>
-                        <span class="view-item-text25">
-                            &gt;
-                            <?php echo $row['description']; ?>
-                        </span>
+                        <span class="view-item-text25">&gt;<?php echo $row['description']; ?></span>
                     </div>
                     <div class="view-item-container12"></div>
-                    <div class="view-item-container13">
-                        <span class="view-item-text26">
-                            <span>Reviews</span>
+                    <div class="view-item-container10">
+                        <span class="view-item-text16">
+                            <span class="view-item-text17">Reviews</span><br />
+                        </span>
+                        <span class="view-item-text33">
+                        <span><?php if($row3["average_value"]){echo "Average : " . $row3["average_value"] . "stars";}?></span>
                             <br />
                         </span>
-                        <?php
-    
-                        if (!empty($itemReview)) {
-                            ?>
-                            <span class="view-item-text29">
-                                <span>Average Rating:</span>
-                                <span>
-                                <?php echo $row3["average_value"] ?> stars
-                                </span>
-                                <br />
-                            </span>
-                            <span class="view-item-text33">
-                                <span>(
-                                    <?php echo $totalReviews; ?> Reviews)
-                                </span>
-                                <br />
-                            </span>
-                            <?php
-
-                            // Display individual reviews
-                            for ($i = 0; $i < min(3, count($itemReview)); $i++) {
-                                ?>
-                                <div class="view-item-container14">
+                        <span class="view-item-text33">
+                        <span><?php echo $row3["Review_count"] ?> Reviews</span>
+                            <br />
+                        </span>
+                    </div>
+                    </div>
+            
+            <?php if($itemAverageRating){?>
+       
+            <div class="container-overflow">
+            <div class="item-review-container">
+            <?php
+             }
+                if($itemRating){
+                foreach($itemRating as $row4){?>
+                    <div class="view-item-container14">
                                     <img alt="image" src="..<?php echo $row4["image_path"] ?>" class="view-item-image2" />
                                     <div class="view-item-container15">
                                         <div class="view-item-container16">
@@ -241,46 +194,26 @@ require_once __DIR__ . '/../sellerAuth.php';
                                         </span>
                                     </div>
                                 </div>
-                                <?php
-                            }
-                        } else {
-                            echo "No reviews";
-                        }
-                        ?>
-                    </div>
-                    <div class="button-separator">
-                        <div class="button-column">
-                    <button class="view-item-button" id="viewMoreBtn"
-                        onclick="window.location='viewItemReviews.php?item_id=<?php echo $item_id; ?>'">
-                        <span class="view-item-text13">
-                            View More
-                        </span>
-                    </button>
-                    </div>
-                    <div class="button-column">
-                    <button class="view-item-button" id="viewMoreBtn"
-                        onclick="window.location='editItem.php?item_id=<?php echo $item_id; ?>'">
-                        <span class="view-item-text13">
-                           Update item
-                        </span>
-                    </button>
-                    </div>
-                    <div class="button-column">
-                        <span class="view-item-text13">
-                        <form action='#' method="POST" id="deleteItemForm">
-										<button  class="view-item-button" id="viewMoreBtn" type="submit" name="deleteItem">Delete Item</button>
-										<input type="hidden" name="item_id" value="<?php echo $item_id; ?>">
-									</button></form>
-                        </span>
-                    </button>
-                    </div>
-                    </div>
-                </div>
+                    <?php }}?>
             </div>
-        </div>
-    </div>
+            </div>
+                <div class="centering-div">
+                <table class ="seller-edit-setting-table">
+                    <tr>
+                    <td class="seller-edit-setting-button-td">
+                    <a href="editItem.php?item_id=<?php echo $item_id; ?>"><button  class="seller-setting-button" type="submit" name="editItemForm">Update Item</button>
+					<input type="hidden" name="item_id" value="<?php echo $item_id; ?>"></span>
+                    </td>
+                    <td class="seller-edit-setting-button-td">
+                    <form action='#' method="POST" id="deleteItemForm">
+										<button  class="seller-setting-button1" type="submit" name="deleteItem">Delete Item</button>
+										<input type="hidden" name="item_id" value="<?php echo $item_id; ?>">
+									</form></button>         
 
-   
-</body>
+    <?php
+        }
+  ?>
  
+
+</body>
 </html>
